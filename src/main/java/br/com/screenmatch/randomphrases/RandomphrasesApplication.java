0d6039963b9dfc9e6651cad0b8dcd13c282766b0
Fabio Mendonça <fabio.mendonca@totvs.com.br>
2024-04-19 15:29:58 -0300

    init: project startup

---
+++ src/main/java/br/com/screenmatch/randomphrases/RandomphrasesApplication.java
@@ -0,0 +1,13 @@
+package br.com.screenmatch.randomphrases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomphrasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomphrasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/screenmatch/randomphrases/RandomphrasesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.screenmatch.randomphrases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomphrasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
